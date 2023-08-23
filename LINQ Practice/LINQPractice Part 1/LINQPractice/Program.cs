ed008992b86bc5adac247de761293b1bebf2710a
Efeincefikir <73502211+Efeincefikir@users.noreply.github.com>
2023-08-23 16:43:15 +0300

    Add files via upload

---
+++ LINQ Practice/LINQPractice Part 1/LINQPractice/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using TCPData;
+using TCPExtentions;
+using System.Linq;
+
+namespace LINQPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Employee> employees = Data.GetEmployees();
+
+            //var filteredEmployees = employees.Where( emp => emp.AnnualSalary < 50000);
+
+            //foreach (var employee in filteredEmployees)
+            //{
+            //    Console.WriteLine($"Id: {employee.Id}");
+            //    Console.WriteLine($"Name: {employee.FirstName}");
+            //   Console.WriteLine($"Surname: {employee.LastName}");
+            //    Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+            //    Console.WriteLine($"Department Id: {employee.DepartmentId}");
+            //    Console.WriteLine("");
+
+            //}
+
+            //List<Department> departments = Data.GetDepartments();
+
+            //var filteredDepartment = departments.Where(emp => emp.ShortName == "HR");
+
+            //foreach (var department in filteredDepartment)
+            //{
+            //    Console.WriteLine($"Id: {department.Id}");
+            //    Console.WriteLine($"Short Name: {department.ShortName}");
+            //    Console.WriteLine($"Long Name: {department.LongName}");
+
+            //    Console.WriteLine("");
+
+            //}
+
+            List<Employee> employees = Data.GetEmployees();
+            List<Department> departments = Data.GetDepartments();
+
+            var resultList = from emp in employees
+                             join dept in departments
+                             on emp.DepartmentId equals dept.Id
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Department = dept.ShortName,
+
+
+                             };
+
+            foreach (var employee in resultList)
+            {
+                Console.WriteLine($"Name: {employee.FirstName}");
+                Console.WriteLine($"Surname: {employee.LastName}");
+                Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+                Console.WriteLine($"Department: {employee.Department}");
+                Console.WriteLine("");
+             }
+
+            var AvgAnnualSalary = resultList.Average(a => a.AnnualSalary);
+            var MaxAnnualSalary = resultList.Max(a => a.AnnualSalary);
+            var MinAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+            Console.WriteLine($" Average Annual Salary is: {AvgAnnualSalary}");
+            Console.WriteLine($" Maximum Annual Salary is: {MaxAnnualSalary}");
+            Console.WriteLine($" Minimum Annual Salary is: {MinAnnualSalary}");
+
+
+        }
+
+
+    }
+}
+
---
+++ LINQ Practice/LINQPractice Part 1/TCPData/Data.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public static class Data
+    {
+        public static List<Employee> GetEmployees()
+
+        {
+            var employees = new List<Employee>();
+            var employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Efe",
+                LastName = "Incefikir",
+                AnnualSalary = 40000,
+                IsManager = true,
+                DepartmentId = 1,
+
+            };
+
+            employees.Add(employee);
+
+
+            employee = new Employee
+            {
+                Id = 2,
+                FirstName = "Lebüs",
+                LastName = "Mebüs",
+                AnnualSalary = 50000,
+                IsManager = false,
+                DepartmentId = 2,
+
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "Tankut",
+                LastName = "Basvara",
+                AnnualSalary = 30000,
+                IsManager = false,
+                DepartmentId = 3,
+
+            };
+            employees.Add(employee);
+
+
+            employee = new Employee
+            {
+                Id = 4,
+                FirstName = "Bankut",
+                LastName = "Tasvara",
+                AnnualSalary = 20000,
+                IsManager = true,
+                DepartmentId = 3,
+
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 5,
+                FirstName = "Leblebi",
+                LastName = "Boy",
+                AnnualSalary = 1020000,
+                IsManager = true,
+                DepartmentId = 4,
+
+            };
+            employees.Add(employee);
+
+
+            return employees;
+        }
+
+        public static List<Department> GetDepartments() {
+        List<Department> departments = new List<Department>();
+        Department department = new Department() {
+
+            Id = 1,
+            ShortName = "HR",
+            LongName = "Human Resources",
+        };
+        departments.Add(department);
+
+            department = new Department()
+            {
+
+                Id = 2,
+                ShortName = "FN",
+                LongName = "Finance",
+            };
+            departments.Add(department);
+
+            department = new Department()
+            {
+
+                Id = 3,
+                ShortName = "TE",
+                LongName = "Technology",
+            };
+            departments.Add(department);
+
+            department = new Department()
+            {
+
+                Id = 4,
+                ShortName = "RND",
+                LongName = "Research and Development",
+            };
+            departments.Add(department);
+
+            return departments;
+
+        }
+
+
+    }
+}
